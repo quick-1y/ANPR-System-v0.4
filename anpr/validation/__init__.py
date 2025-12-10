f25fa9d519ac96a9130e56e679ff71194eb5d313
quick-1y <l0nglife3025@gmail.com>
2025-12-10 09:17:40 +0300

    Delete anpr directory

--- anpr/validation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Постобработка и валидация распознанных автомобильных номеров."""
-
-from .plate_validator import PlateValidationResult, PlateValidator
-
-__all__ = ["PlateValidationResult", "PlateValidator"]
